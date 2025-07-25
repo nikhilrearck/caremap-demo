7c7fec9363719a6a3ffdb271d0ba63eff16cc41d
nikhilrearck <nikhil.khetan@btsavvy.net>
2025-07-25 22:59:47 +0530

    changes icon for high-level-goals

--- app/home/myHealth/notes/index.tsx
+++ app/home/myHealth/notes/index.tsx
@@ -245,7 +245,7 @@ function AddNotesPage({
 
   const [showDatePicker, setShowDatePicker] = useState(false);
 
-  // Helper to format date as MM-DD-YY
+  // Helper to format date as MM-DD-YYYY
   const formatDate = (date: Date) => {
     const mm = String(date.getMonth() + 1).padStart(2, "0");
     const dd = String(date.getDate()).padStart(2, "0");
