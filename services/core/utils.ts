32de944ba171cb2cf4b41f938a561a5748b25073
Jaimeen Patel <jaimeen.patel@btsavvy.net>
2025-08-04 20:58:18 +0530

    Enhanced debugging

--- services/core/utils.ts
+++ services/core/utils.ts
@@ -19,7 +19,7 @@ export const pickImageFromLibrary = async (): Promise<ImagePickerResult> => {
         }
 
         const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            mediaTypes: 'images',
             allowsEditing: true,
             aspect: [1, 1],
             quality: 0.5,
