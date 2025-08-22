54a54a0a74a76a2273b7393e33c48ce95eca80e4
Harshad Sawant <harshad.sawant@btsavvy.net>
2025-08-22 22:10:13 +0530

    bugfix: enable foreign_keys after migrations to restore seed data (#48)

--- services/database/db.ts
+++ services/database/db.ts
@@ -19,7 +19,6 @@ const dbReadyPromise = new Promise<SQLiteDatabase>((resolve) => {
 export const initializeDatabase = async (db: SQLiteDatabase): Promise<void> => {
     await handleAndroidDBReset(DB_NAME);
     _db = db;
-    await _db.execAsync("PRAGMA foreign_keys = ON;");
     dbReadyResolver?.(db);
     logger.debug(`DB Path: "${_db.databasePath}"`);
     await runMigrations(_db);
@@ -57,4 +56,6 @@ export const runMigrations = async (db: SQLiteDatabase): Promise<void> => {
             await db.execAsync(`PRAGMA user_version = ${DB_VERSION}`);
         });
     }
+
+    await db.execAsync("PRAGMA foreign_keys = ON;");
 };
